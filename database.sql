b6160bb6280b2031e36d37b7f0fc72ef08d40b96
omshinde12 <146666701+omshinde12@users.noreply.github.com>
2023-10-01 23:58:27 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,20 @@
+create database register;
+
+
+use register;
+
+
+CREATE TABLE `user_information` (
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+select * from user_information;
+
